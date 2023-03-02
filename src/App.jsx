799447d59d56d9ac39a567f2b8f2d4db473efa54
@Jonath-z <jonathanzihindula95@gmail.com>
2023-03-02 16:51:14 +0200

    Revert "add CRUD operation"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import InputBar from "./components/InputBar";
-import TodosContainer from "./components/TodosContainer";
-
 function App() {
-  return (
-    <div className="w-screen bg-orange-100 h-screen overflow-y-auto">
-      <div className="max-w-3xl pt-20 mx-auto px-5">
-        <InputBar />
-        <TodosContainer />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import todoReducer from "./features/todo";
-
-const store = configureStore({
-  reducer: {
-    todo: todoReducer,
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
